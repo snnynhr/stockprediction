14b9b47cdc323422d021af7d16efd77a869e4c7c
xiaote <xiaotez@andrew.cmu.edu>
2014-10-21 12:41:17 -0400

    add word dictionary

---
+++ data/dict/dict.py
@@ -0,0 +1,32 @@
+import json
+import os
+import nltk
+
+dowjones = "../dowjones"
+
+wordD = dict()
+
+def countwords(w):
+	if w not in wordD:
+		wordD[w] = 0
+	wordD[w] += 1
+
+def countHeadlines(h):
+	sent = h["Headline"]
+	words = nltk.word_tokenize(sent)
+	map(countwords,words)
+
+
+for fname in os.listdir(dowjones):
+	if fname.endswith('json'):
+		jfile = open(dowjones+'/' + fname,'r+')
+		d = json.load(jfile)
+		headlines = d["Headlines"]
+		map(countHeadlines, headlines)
+		jfile.close()
+
+sortedD = sorted(wordD.items(),key=(lambda (k,v):v), reverse = True)
+dictFile = open('dict.json','w')
+dictFile.write(json.dumps(sortedD))
+dictFile.close()
+
