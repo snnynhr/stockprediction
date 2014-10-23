c5e788da9877ed7dedc43f8aff82851a4341ac31
Sunny Nahar <sunnyn@cmu.edu>
2014-10-23 01:07:30 -0400

    updated data

--- data/training/csvtojson.py
+++ data/training/csvtojson.py
@@ -1,15 +1,19 @@
 import json
 import os
-import time
 
 dowjones = "./sp500"
 filename = "tickerStockTrain.json"
-out = open(filename,'w')
+debug = "debug.txt"
+out = open(filename, 'w')
+debug = open(debug, 'w')
 for fname in os.listdir(dowjones):
 	if fname.endswith('csv'):
 		store = dict()
-		f = open(dowjones+'/' + fname,'r+')
+		f = open(dowjones+'/' + fname, 'r+')
 		l = f.readlines()
+		if l[0].split(",")[0] != "Date":
+			debug.write("No data for " + fname + "\n")
+			continue
 		for i in range(1, len(l)):
 			p = l[i].split(",")
 			date = p[0]
@@ -17,7 +21,8 @@ for fname in os.listdir(dowjones):
 			dates = date.split("-")
 			d = "".join(dates)
 			store[d] = price
-		out.write(fname[0:-4]+"\t"+json.dumps(store, sort_keys = True))
+		out.write(fname[0:-4]+"\t"+json.dumps(store, sort_keys=True))
 		out.write("\n")
-		print "Converted " + fname
+		debug.write("Converted " + fname + "\n")
 out.close()
+debug.close()
