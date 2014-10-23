39efbb75a6636a3bd19b87138aa4789e51d48fc4
xiaote <xiaotez@andrew.cmu.edu>
2014-10-22 23:48:36 -0400

    for using creg

---
+++ src/creg_driver.py
@@ -0,0 +1,41 @@
+import json
+import subprocess
+
+
+# Trains a model and save it to `weights`.
+def train((train_features, train_labels), weights=None, options={}, quiet=False):
+  p = subprocess.Popen(['creg', '-x', train_features, '-y', train_labels] + (['-W'] if weights is None else ['--z', weights]) + [a for item in options.iteritems() for a in item if a], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+  output, err = p.communicate()
+
+  if p.returncode != 0: raise UserWarning('creg returned {}'.format(p.returncode))
+
+  return output, err
+#end def
+
+
+# Train and evaluate. Does not store weights. Just returns a list of posteriors and their true labels.
+def evaluate((train_features, train_labels), (test_features, test_labels), options={}):
+  new_options = options
+  new_options['--tx'] = test_features
+  new_options['--ty'] = test_labels
+  new_options['--write_test_distribution'] = ''
+
+  ground_truth = {}
+  with open(test_labels) as f:
+    for line in f:
+      key, label = line.strip('\n').split('\t')
+      ground_truth[key] = label
+    #end for
+  #end with
+
+  output, err = train((train_features, train_labels), options=new_options)
+
+  posteriors = {}
+  for line in output.split('\n'):
+    if not line.strip(): continue
+    key, pred, posterior_json = line.strip().split('\t', 3)
+    posteriors[key] = {'true_label': ground_truth[key], 'predicted_label': pred, 'posterior': json.loads(posterior_json)}
+  #end for
+
+  return posteriors
+#end def
