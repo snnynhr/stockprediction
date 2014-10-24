11635485af3f19f81985fce19e0de5c20f60ee69
xiaote <xiaotez@andrew.cmu.edu>
2014-10-24 15:40:57 -0400

    add weightfiles and update src code

--- src/creg_driver.py
+++ src/creg_driver.py
@@ -4,7 +4,7 @@ import subprocess
 
 # Trains a model and save it to `weights`.
 def train((train_features, train_labels), weights=None, options={}, quiet=False):
-  p = subprocess.Popen(['creg', '-x', train_features, '-y', train_labels] + (['-W'] if weights is None else ['--z', weights]) + [a for item in options.iteritems() for a in item if a], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+  p =subprocess.Popen(['creg', '-x', train_features, '-y', train_labels] + (['-W'] if weights is None else ['--z', weights]) + [a for item in options.iteritems() for a in item if a], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
   output, err = p.communicate()
 
   if p.returncode != 0: raise UserWarning('creg returned {}'.format(p.returncode))
@@ -19,6 +19,11 @@ def evaluate((train_features, train_labels), (test_features, test_labels), optio
   new_options['--tx'] = test_features
   new_options['--ty'] = test_labels
   new_options['--write_test_distribution'] = ''
+  weights = None
+  if '--z' in options:
+    weights = options['--z']
+    del options['--z']
+  #end if
 
   ground_truth = {}
   with open(test_labels) as f:
@@ -28,7 +33,7 @@ def evaluate((train_features, train_labels), (test_features, test_labels), optio
     #end for
   #end with
 
-  output, err = train((train_features, train_labels), options=new_options)
+  output, err = train((train_features, train_labels), weights=weights, options=new_options)
 
   posteriors = {}
   for line in output.split('\n'):
---
+++ src/regMainB.py
@@ -0,0 +1,51 @@
+import os
+import random
+import json
+import creg_driver
+import operator
+import sys
+
+datadir = '../XYdata/2'
+fulldir = datadir + '/full'
+testdir = datadir + '/test'
+traindir = datadir + '/train'
+devdir = datadir + '/dev'
+stockfile = open(datadir + '/' + 'stock.txt','r')
+stockList = stockfile.readline().strip().split('\t')
+print "there are", len(stockList), "stocks"
+stockfile.close()
+
+
+l1 = sys.argv[1]
+print l1
+
+weightdir = '%s/weight%s' %(datadir, l1)
+if not os.path.exists(weightdir):
+    os.makedirs(weightdir)
+
+resultfile = open('%s/resultl1=%s.txt' %(datadir, l1), 'w')  
+
+for stock in stockList:
+    print "learning for %s ..." %stock
+    trainXPath = traindir + '/' + stock + 'x'
+    trainYPath = traindir + '/' + stock + 'y'
+
+    testXPath = testdir + '/' + stock + 'x'
+    testYPath = testdir + '/' + stock + 'y'
+
+    devXPath = devdir + '/' + stock + 'x'
+    devYPath = devdir + '/' + stock + 'y'
+
+    weightPath = '%s/%s.txt' %(weightdir, stock)
+    weightFile = open(weightPath, 'a')
+    weightFile.close() 
+     
+    results = creg_driver.evaluate((trainXPath, trainYPath), (devXPath, devYPath), options={"--l1": l1, "--z":weightPath})
+    accuracy = round(float(sum(map(lambda v: 1 if v['true_label'] == v['predicted_label'] else 0, results.values()))) / len(results),4)
+
+    resultfile.write('%s\t%d\n' %(stock, accuracy))
+    print stock, accuracy
+
+resultfile.close()
+       
+
