8f6fc5e05f445ad75802eee165316fa69722cbdd
xiaote <xiaotez@andrew.cmu.edu>
2014-10-31 00:41:09 -0400

    updated src code

--- src/XYmain.py
+++ src/XYmain.py
@@ -5,6 +5,15 @@ import json
 import os
 import copy
 
+# change here for new folders
+XYdir = "../XYdata/5/full"
+Ypath = "../data/stocks/tickerStock.json"
+Xdir = "../data/dowjones"
+
+if not os.path.exists(XYdir):
+    os.mkdir(XYdir)
+# change here too
+readmePath = XYdir + '/README.md'
 
 def pruneX(Xdict, start, end):
     for date in Xdict.keys():
@@ -52,6 +61,7 @@ def combineS(Xdict, YList):
             X.append(dict())
     return X, Y, D
 
+# destructively modify D1
 def mergeFeatures(D1, D2):
     for key in D2:
         if key in D1:
@@ -59,281 +69,109 @@ def mergeFeatures(D1, D2):
         else:
             D1[key] = D2[key]
 
-
-# change here for new folders
-XYdir = "../XYdata/5/full"
-Ypath = "../data/stocks/tickerStock.json"
-Xdir = "../data/dowjones"
-
-if not os.path.exists(XYdir):
-    os.mkdir(XYdir)
-# change here too
-readmePath = XYdir + '/README.md'
-
-# write documentation in md format
-readmeContent = \
-"""
-featuresBMs + featureCs + discrete
-"""
-
-if not os.path.isfile(readmePath):
-    readmefile = open(readmePath, 'w')
-    readmefile.write(readmeContent)
-    readmefile.close()
-
-def mainA():
-    Yfile = open(Ypath,'r')
-    for line in Yfile:
-        info = line.split('\t')
-        if len(info) == 2:
-            stock = info[0]
-            print "building XYfile for %s ..." %stock
-            prices = json.loads(info[1])
-            YList = Ylabel.discrete(prices)
-            Xpath = Xdir + '/' + stock + '.json'
-            if os.path.isfile(Xpath):
-                print "extracting features ..."
-                Xfile = open(Xpath, 'r')
-                XD = json.load(Xfile)
-                Xdict, dim = features.featureAs(XD)
-                # for date in X:
-                #     s = sum(X[date])
-                #     if s != 0 :
-                #         print date, s
-                if len(Xdict) < 1:
-                    print "no headline data"
-                    continue
-
-                start_Y = YList[0][0]
-                end_Y = YList[-1][0]
-                start_X = min(Xdict.keys())
-                end_X = max(Xdict.keys())
-                print "time range of stock prices: %s - %s" %(start_Y, end_Y)
-                print "time range of headlines: %s - %s" %(start_X, end_X)
-                #caution: X and Y have to use the format of %Y%m%d
-                start_date = max(start_X, start_Y)
-                end_date = min(end_X, end_Y)
-
-                if start_date>= end_date:
-                    print "not enough data"
-                    continue
-
-                print "time range of data: %s - %s" %(start_date, end_date)
-                
-                Xdict = pruneX(Xdict, start_date, end_date)
-                YList = pruneY(YList, start_date, end_date)
-
-                X, Y, Date = combineS(Xdict, YList, dim)
-
-                #print [sum(x) for x in X]
-                #print Y
-                totalcount = len(Y)
-                print "there are %d data points ..." % totalcount
-
-                XYPath = XYdir + '/' + stock + '.json'
-                XYfile = open(XYPath, 'w')
-                for i in xrange(totalcount):
-                    XYfile.write(json.dumps((Date[i],Y[i],X[i])) + '\n')
-                XYfile.close()
-
-            else:
-                print "no headline file"
-
-def mainB():
-    Yfile = open(Ypath,'r')
-    print "extracting features ..."
+def crossMarketFeatures(stockList,fnList1, Ylists):
     featureD = dict()
-    for line in Yfile:
-        info = line.split('\t')
-        if len(info) == 2:
-            stock = info[0]
-            Xpath = Xdir + '/' + stock + '.json'
-            if os.path.isfile(Xpath):
-                print "including headlines from %s ..." %stock
-                Xfile = open(Xpath, 'r')
-                XD = json.load(Xfile)
-                features.featureBMs(featureD,XD,stock)
+    for i in xrange(len(stockList)):
+        stock = stockList[i]
+        Ylist = Ylist[i]
+        Xpath = Xdir + '/' + stock + '.json'
+        if os.path.isfile(Xpath):
+            Xfile = open(Xpath, 'r')
+            Xdict = json.load(Xfile)
+            for fn in fnList1:
+                print "including %s from %s ..." %(str(fn),stock)
+                features.fn(featureD, Xdict, Ylist, stock)
+    return featureD
 
-    Yfile.close()
+def singleStockFeature(stock, fnList2, Ylist):
+    featureD = dict()
+    Xpath = Xdir + '/' + stock + '.json'
+    if os.path.isfile(Xpath):
+        Xfile = open(Xpath, 'r')
+        Xdict = json.load(Xfile)
+        for fn in fnList2:
+            print "including %s for %s ..." %(str(fn),stock)
+            features.fn(featureD, Xdict, Ylist, stock)
+        return featureD
+    else:
+        return None
+
+# fnList1 for features that are specfic to a stock
+# fnList2 for features that are the same to all stocks
+def main(fnList1,fnList2, overWriting = True):
+    print "extracting features ..."
+
     Yfile = open(Ypath,'r')
-
+    stockList = []
+    Ylists = []
     for line in Yfile:
         info = line.split('\t')
-        if len(info) == 2:
-            stock = info[0]
-            if stock > 'UPS':
-                Xdict = copy.deepcopy(featureD)
-                print "building XYfile for %s ..." %stock
-                prices = json.loads(info[1])
-                YList = Ylabel.discrete(prices)
-
-                start_Y = YList[0][0]
-                end_Y = YList[-1][0]
-                start_X = min(Xdict.keys())
-                end_X = max(Xdict.keys())
-
-                #caution: X and Y have to use the format of %Y%m%d
-                start_date = max(start_X, start_Y)
-                end_date = min(end_X, end_Y)
-
-                if start_date>= end_date:
-                    print "not enough data"
-                    continue
-
-                print "time range of data: %s - %s" %(start_date, end_date)
-                
-                Xdict = pruneX(Xdict, start_date, end_date)
-                YList = pruneY(YList, start_date, end_date)
-
-                X, Y, Date = combineS(Xdict, YList)
-
-                #print [sum(x) for x in X]
-                #print Y
-                totalcount = len(Y)
-                print "there are %d data points ..." % totalcount
-
-                XYPath = XYdir + '/' + stock + '.json'
-
-                if totalcount < 1000:
-                    print "not enough data points"
-                    if os.path.isfile(XYPath):
-                        os.remove(XYPath)
-                        print  "remove %s" %(XYPath)
-                    continue
-
-                if os.path.isfile(XYPath):
-                    print "file already exists"
-                    continue
-                else:
-                    XYfile = open(XYPath, 'w')
-                    for i in xrange(totalcount):
-                        XYfile.write(json.dumps((Date[i],Y[i],X[i])) + '\n')
-                    XYfile.close()
-                    print "creating new files"
-
-def mainC():
-    Yfile = open(Ypath,'r')
-    for line in Yfile:
-        info = line.split('\t')
-        if len(info) == 2:
-            stock = info[0]
-            print "building XYfile for %s ..." %stock
+        if len(info) ==2:
+            stockList.append(info[0])
             prices = json.loads(info[1])
-            YList = Ylabel.discrete(prices)
+            Ylists.append(Ylabel.discrete(prices))
+    Yfile.close()
+    
+    fnNames = map(str, fnList1 + fnList2)
+    readmeContent = '+'.join(fnNames)
+    if not os.path.isfile(readmePath):
+        readmefile = open(readmePath, 'w')
+        readmefile.write(readmeContent)
+        readmefile.close()
 
-            Xdict = features.featureCs(YList,5)
+    featureDGeneral = crossMarketFeatures(stockList,fnList1, YLists)
 
-            start_Y = YList[0][0]
-            end_Y = YList[-1][0]
-            start_X = min(Xdict.keys())
-            end_X = max(Xdict.keys())
+    for i in xrange(len(stockList)):
+        stock = stockList[i]
+        Ylist = YLists[i]
+        print "building XYfile for %s ..." %stock
+        featureD = singleStockFeature(stock, fnList2, Ylist)
+        if featureD == None:
+            print "no headline file"
+            continue
+        mergeFeatures(featureD, featureDGeneral)
 
-            #caution: X and Y have to use the format of %Y%m%d
-            start_date = max(start_X, start_Y)
-            end_date = min(end_X, end_Y)
+        start_Y = Ylist[0][0]
+        end_Y = Ylist[-1][0]
+        start_X = min(featureD.keys())
+        end_X = max(featureD.keys())
 
-            if start_date>= end_date:
-                print "not enough data"
-                continue
+        #caution: X and Y have to use the format of %Y%m%d
+        start_date = max(start_X, start_Y)
+        end_date = min(end_X, end_Y)
 
-            print "time range of data: %s - %s" %(start_date, end_date)
-                
-            Xdict = pruneX(Xdict, start_date, end_date)
-            YList = pruneY(YList, start_date, end_date)
+        if start_date>= end_date:
+            print "not enough data"
+            continue
 
-            X, Y, Date = combineS(Xdict, YList)
+        print "time range of data: %s - %s" %(start_date, end_date)
 
-            totalcount = len(Y)
-            print "there are %d data points ..." % totalcount
+        featureD = pruneX(featureD, start_date, end_date)
+        Ylist = pruneY(Ylist, start_date, end_date)
 
-            XYPath = XYdir + '/' + stock + '.json'
+        X, Y, date = combineS(featureD, Ylist)
 
-            if totalcount < 1000:
-                print "not enough data points"
-                if os.path.isfile(XYPath):
-                    os.remove(XYPath)
-                    print  "remove %s" %(XYPath)
-                continue
+        totalcount = len(Y)
+        print "there are %d data points ..." % totalcount
 
+        XYPath = XYdir + '/' + stock + '.json'
+
+        if totalcount < 1000:
+            print "not enough data points"
+            if os.path.isfile(XYPath):
+                os.remove(XYPath)
+                print  "remove %s" %(XYPath)
+            continue
+
+        if not overWriting and os.path.isfile(XYPath):
+            print "file already exists"
+            continue
+        
+        else:
             XYfile = open(XYPath, 'w')
+            print "creating new files..."
             for i in xrange(totalcount):
                 XYfile.write(json.dumps((Date[i],Y[i],X[i])) + '\n')
             XYfile.close()
-            print "creating new files"
-
-def mainD():
-    Yfile = open(Ypath,'r')
-    print "extracting features ..."
-    featureD = dict()
-    for line in Yfile:
-        info = line.split('\t')
-        if len(info) == 2:
-            stock = info[0]
-            Xpath = Xdir + '/' + stock + '.json'
-            if os.path.isfile(Xpath):
-                print "including headlines from %s ..." %stock
-                Xfile = open(Xpath, 'r')
-                XD = json.load(Xfile)
-                features.featureBMs(featureD,XD,stock)
-
-    Yfile.close()
-    Yfile = open(Ypath,'r')
-
-    for line in Yfile:
-        info = line.split('\t')
-        if len(info) == 2:
-            stock = info[0]
-            Xdict = copy.deepcopy(featureD)
-            print "building XYfile for %s ..." %stock
-            prices = json.loads(info[1])
-            YList = Ylabel.discrete(prices)
-            Xdict1 = features.featureCs(YList,5)
-            mergeFeatures(Xdict, Xdict1)
-
-            start_Y = YList[0][0]
-            end_Y = YList[-1][0]
-            start_X = min(Xdict.keys())
-            end_X = max(Xdict.keys())
-
-            #caution: X and Y have to use the format of %Y%m%d
-            start_date = max(start_X, start_Y)
-            end_date = min(end_X, end_Y)
-
-            if start_date>= end_date:
-                print "not enough data"
-                continue
-
-            print "time range of data: %s - %s" %(start_date, end_date)
-            
-            Xdict = pruneX(Xdict, start_date, end_date)
-            YList = pruneY(YList, start_date, end_date)
-
-            X, Y, Date = combineS(Xdict, YList)
-
-            #print [sum(x) for x in X]
-            #print Y
-            totalcount = len(Y)
-            print "there are %d data points ..." % totalcount
-
-            XYPath = XYdir + '/' + stock + '.json'
-
-            if totalcount < 1000:
-                print "not enough data points"
-                if os.path.isfile(XYPath):
-                    os.remove(XYPath)
-                    print  "remove %s" %(XYPath)
-                continue
-
-            if os.path.isfile(XYPath):
-                print "file already exists"
-                continue
-            else:
-                XYfile = open(XYPath, 'w')
-                for i in xrange(totalcount):
-                    XYfile.write(json.dumps((Date[i],Y[i],X[i])) + '\n')
-                XYfile.close()
-                print "creating new files"
-
-mainD()
 
 
--- src/features.py
+++ src/features.py
@@ -11,48 +11,10 @@ dictList = json.load(dictFile)
 D = dict([(w[0], i) for i, w in enumerate(dictList)])
 dim = len(dictList)
 
-# for dense feature vectors: X is a dictionary of list
-def featureA(XD):
-	headlines = XD["Headlines"]
-	X = dict()
-	for h in headlines:
-		sent = h["Headline"]
-		words = nltk.word_tokenize(sent)
-		c_time_str = h["CreateTimestamp"]["Value"]
-		c_time = time.strptime(c_time_str, "%Y-%m-%dT%H:%M:%S")
-		c_date_str = time.strftime("%Y%m%d", c_time)
-		if c_date_str not in X:
-			X[c_date_str] = [0] * dim
-		for w in words:
-			if w in D:
-				X[c_date_str][D[w]] += 1
-	return X, dim
-
-# for sparse ones: X is a dictionary of dictionary
-def featureAs(XD, extra=''):
-	headlines = XD["Headlines"]
-	X = dict()
-	for h in headlines:
-		sent = h["Headline"]
-		words = nltk.word_tokenize(sent)
-		c_time_str = h["CreateTimestamp"]["Value"]
-		c_time = time.strptime(c_time_str, "%Y-%m-%dT%H:%M:%S")
-		c_date_str = time.strftime("%Y%m%d", c_time)
-		if c_date_str not in X:
-			X[c_date_str] = dict()
-		for w in words:
-			if w in D:
-				key = str(D[w]) + extra
-				if key in X[c_date_str]:
-					X[c_date_str][key] += 1
-				else:
-					X[c_date_str][key] = 1
-	return X, dim
-
-# for sparse ones: X is a dictionary of dictionary
-def featureAMs(XD, extra=''):
-	headlines = XD["Headlines"]
-	X = dict()
+# for sparse ones: featureD is a dictionary of dictionary
+# headlines are all before 9:30 EST
+def featureAMs(featureD, Xdict, Ylist, stock):
+	headlines = Xdict["Headlines"]
 	for h in headlines:
 		sent = h["Headline"]
 		words = nltk.word_tokenize(sent)
@@ -62,44 +24,18 @@ def featureAMs(XD, extra=''):
 			c_date_str = (c_time + datetime.timedelta(days=1)).strftime("%Y%m%d")
 		else:
 			c_date_str = c_time.strftime("%Y%m%d")
-		if c_date_str not in X:
+		if c_date_str not in Xdict:
 			X[c_date_str] = dict()
 		for w in words:
 			if w in D:
-				key = str(D[w]) + extra
+				key = "%s_%d" % (stock,D[w])
 				if key in X[c_date_str]:
 					X[c_date_str][key] += 1
 				else:
 					X[c_date_str][key] = 1
-	return X, dim
 
-# headlines from all stocks
-def featureBs(featureD, XD, stock):
-	X, dim = featureAs(XD, stock)
-	if len(X) < 1:
-		print "not enough headlines"
-		return
-	for key in X:
-		if key in featureD:
-			featureD[key].update(X[key])
-		else:
-			featureD[key] = X[key]
-
-# headlines from all stocks  (before 9:30 EST)
-def featureBMs(featureD, XD, stock):
-	X, dim = featureAMs(XD, stock)
-	if len(X) < 1:
-		print "not enough headlines"
-		return
-	for key in X:
-		if key in featureD:
-			featureD[key].update(X[key])
-		else:
-			featureD[key] = X[key]
 
 # Ylabels from the previous states
-def featureCs(Ylist, count = 3):
-	featureD = dict()
+def featureCs(featureD, Xdict, Ylist, stock, count = 5):
 	for i in xrange(count, len(Ylist)):
-		featureD[Ylist[i][0]] = dict(map(lambda x : (-x, Ylist[i-x][1]), xrange(1, count+1)))
-	return featureD
+		featureD[Ylist[i][0]] = dict(map(lambda x : ("%s_%d" %(stock, -x), Ylist[i-x][1]), xrange(1, count+1)))
